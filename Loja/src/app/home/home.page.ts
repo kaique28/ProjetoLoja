5f1721d05d49d8ee4fd105ecfc00e20fd1075b8f
KAIQUE CARVALHO SILVA <kaique.csilva8@senacsp.edu.br>
2019-12-05 12:28:58 -0300

    Adição do link na página home para detalheproduto

--- Loja/src/app/home/home.page.ts
+++ Loja/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -21,7 +22,12 @@ export class HomePage {
   usa
   */
   public produtos:Array<Object>=[];
-    constructor(private http:HttpClient) {}
+    constructor(private http:HttpClient , private navCtrl:NavController) {}
+
+    public navDetalheProduto(id:string){
+      this.navCtrl.navigateForward("detalheproduto?id="+id);
+    }
+
 
     /*
     O comando ngOnInit(ng->todos os comando internos do Angular|
